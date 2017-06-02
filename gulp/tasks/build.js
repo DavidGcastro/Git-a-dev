6af440b3b05c44241b5dd277ea552b4a14aa7f42
David <davidgcastro93@gmail.com>
2017-06-01 23:24:45 -0400

    completed gulp build

---
+++ gulp/tasks/build.js
@@ -0,0 +1,95 @@
+var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var del = require ('del');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var cssnano = require("gulp-cssnano");
+var uglify = require ("gulp-uglify");
+var browserSync = require('browser-sync').create();
+
+gulp.task('dist', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "docs"
+    }
+  });
+
+});
+
+
+
+gulp.task('deleteDist',['icons'], function(){
+	return del ("./docs");
+});
+
+gulp.task("copyGeneralFiles", ['deleteDist'], function(){
+	
+	 var pathsToCopy = [
+		 './app/**/*',
+		 '!./app/index.html',
+		 '!./app/assets/images/**',
+		 '!./app/assets/styles/**',
+		 '!./app/assets/scripts/**',
+		 '!./app/temp',
+		 '!./app/temp/**'
+		 
+		 
+	 ]
+	
+	
+	return gulp.src(pathsToCopy)
+	.pipe(gulp.dest("./docs"));
+});
+
+
+
+gulp.task('optimizeImages',['deleteDist'], function(){
+	return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*' ])
+	.pipe(imagemin({
+		progressive: true,
+		interlace: true,
+		multipass:true
+	}))
+	.pipe(gulp.dest('./docs/assets/images'));
+	
+});
+
+
+
+
+gulp.task('useminTrigger', ['deleteDist'], function(){
+	gulp.start('usemin');
+});
+
+
+
+
+
+gulp.task('usemin', ['styles', 'scripts'], function(){
+	return gulp.src(['./app/index.html'])
+      .pipe(usemin({
+		css: [function(){
+			return rev()
+		}, function(){
+		return	cssnano()
+		}],
+		js: [function(){
+			return rev()
+		}, function(){
+			return uglify()
+		}],
+		
+	}))
+      .pipe(gulp.dest('./docs'));
+		  
+		  
+		  });
+
+
+
+
+
+
+gulp.task('build', ['deleteDist', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
